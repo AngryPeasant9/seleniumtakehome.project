18f78fb15e36933a8a801a1addd84d6b4347f974
AngryPeasant9 <30418620+AngryPeasant9@users.noreply.github.com>
2018-06-11 00:39:32 -0500

    Updated README.md

--- src/test/java/FacebookLoginTest.java
+++ src/test/java/FacebookLoginTest.java
@@ -1,6 +1,7 @@
 import static org.testng.Assert.*;
 import org.json.*;
 import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.io.*;
@@ -9,21 +10,18 @@ import java.nio.file.Paths;
 import java.util.regex.Pattern;
 
 
-@Test
+
 public class FacebookLoginTest {
     @Test
     public void testSuccessfulLogin() throws JSONException, IOException {
         //reading entire json
         String entireFile =  new String(Files.readAllBytes(Paths.get("tests.json")));
 
-        System.out.println(entireFile);
-
         JSONObject AllTests = new JSONObject(entireFile);
         JSONArray tests = AllTests.getJSONArray("Tests");
 
         //For loop will repeatedly test so long as we have tests defined in the json.
-        for (int i = 0; i < tests.length(); i++)
-        {
+        for (int i = 0; i < tests.length(); i++) {
             //Grabbing the username and password we wish to test
             String userName = tests.getJSONObject(i).getString("username");
             String password = tests.getJSONObject(i).getString("password");
@@ -43,8 +41,8 @@ public class FacebookLoginTest {
             assertTrue(Pattern.matches(".*Facebook", TestUser.getUserDriver().getTitle()));
 
             //Checking we weren't asked to provide login credentials again.
-            assertEquals(TestUser.getUserDriver().findElements( By.id("email") ).size() == 0, true);
-            assertEquals(TestUser.getUserDriver().findElements( By.id("pass") ).size() == 0, true);
+            assertEquals(TestUser.getUserDriver().findElements(By.id("email")).size() == 0, true);
+            assertEquals(TestUser.getUserDriver().findElements(By.id("pass")).size() == 0, true);
 
             //Checking for the Home link and account settings drop down to verify we are logged in.
             assertEquals(TestUser.getUserDriver().findElements(By.xpath("//*[.=\"Home\"]")).size() != 0, true);
@@ -53,5 +51,9 @@ public class FacebookLoginTest {
             //shutting down this WebDriver
             TestUser.quit();
         }
+    }
+    @AfterMethod
+    public void cleanup() throws IOException {
+        Runtime.getRuntime().exec("taskkill /im chromedriver.exe /f");
     }
 }
